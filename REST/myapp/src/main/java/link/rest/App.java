e1cdce355c240e26d70376b26fc2aa3d04291240
taehyoung <matthew4091@gmail.com>
2015-09-15 19:43:04 +0900

    ..

---
+++ REST/myapp/src/main/java/link/rest/App.java
@@ -0,0 +1,13 @@
+package link.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
