cdf5ceba3692a8adebc55201590c1f47ddc240e6
taehyoung <matthew4091@gmail.com>
2015-09-23 12:30:11 +0900

    ..

--- wsc/src/main/java/koreatech/cse/controller/HomeController.java
+++ wsc/src/main/java/koreatech/cse/controller/HomeController.java
@@ -1,10 +1,23 @@
 package koreatech.cse.controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller @RequestMapping("/") public class HomeController {
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @Value("${env.text}")
+    private String env;
+
     @RequestMapping
     public String home(Model model) {
         model.addAttribute("textFromController", "Worlddss안녕!!d");
-        return "hello"; }
+        return "hello";
+    }
+
+    @RequestMapping("/env")
+    public String env(Model model) {
+        model.addAttribute("textFromController", env);
+        return "hello";
+    }
 }
