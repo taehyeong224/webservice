cbb4c8316f6426f5d4ef564fe8e04039553ce63d
taehyoung <matthew4091@gmail.com>
2015-10-15 21:19:58 +0900

    ..

---
+++ wsc/src/main/java/koreatech/cse/domain/rest/TemperatureXml.java
@@ -0,0 +1,10 @@
+package koreatech.cse.domain.rest;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by kth on 2015. 10. 14..
+ */
+@XmlRootElement(name = "Temperature")
+public class TemperatureXml extends Temperature {
+}
